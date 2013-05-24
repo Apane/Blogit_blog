200a66cc4be06bee9de31c19fa78f0ab4f7d4b87
Apane <Anthony.panepinto101@gmail.com>
2013-05-24 16:56:53 -0400

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  
+  def current_user
+    @current_user ||= User.first || User.create!(name: "APane")
+  end
+  
+  def login_required
+    redirect_to root_url unless current_user
+  end
+  
+  helper_method :current_user
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalBlog::Application.config.secret_token = '19490919c8d1f6db9a7db66e8b12f8dd5410399ffff6b5477da43e64e241a36464c88565da5283375529073c36589722bd2268cd6ebb6a6d1afb4cc12a7b73a7'
