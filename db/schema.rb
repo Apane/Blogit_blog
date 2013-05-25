0b89e7507db1eeb196d291b8b6f0dbced3b5f9ee
Apane <Anthony.panepinto101@gmail.com>
2013-05-25 11:59:29 -0400

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130525075255) do
+ActiveRecord::Schema.define(:version => 20130525155642) do
 
   create_table "blog_comments", :force => true do |t|
     t.string   "name",       :null => false
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(:version => 20130525075255) do
   create_table "users", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "username"
+    t.string   "name"
   end
 
 end
